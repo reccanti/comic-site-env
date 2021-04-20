f880ba72a37ddabbf2884655741332172f94bdb2
Blair Wilcox <reccanti@gmail.com>
2021-04-19 22:53:22 -0400

    update clone plugins

--- scripts/clone-plugins.sh
+++ scripts/clone-plugins.sh
@@ -5,8 +5,12 @@
 
 # Webcomic Plugin
 git clone https://github.com/mgsisk/webcomic.git ./tmp/webcomic
-mv ./tmp/webcomic/* ./wp-app/wp-content/plugins/
+mkdir ./wp-app/wp-content/plugins/webcomic/
+mv ./tmp/webcomic/src/* ./wp-app/wp-content/plugins/webcomic/
 rm -rf ./tmp
 
 # WP Migrations Plugin
 git clone https://github.com/cuisine-wp/wp-table-migrations.git ./wp-app/wp-content/plugins/wp-table-migrations
+
+# Apple 2000 Blocks
+git clone https://github.com/reccanti/apple2000-shamncysection.git ./wp-app/wp-content/plugins/apple2000-shamncysection
